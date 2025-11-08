6d1289147d6676318b4188fff81c7645bb42ed21
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2025-11-08 19:46:25 +0100

    mix new spectral --app spectral

---
+++ lib/spectral.ex
@@ -0,0 +1,18 @@
+defmodule Spectral do
+  @moduledoc """
+  Documentation for `Spectral`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Spectral.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spectral_test.exs
@@ -0,0 +1,8 @@
+defmodule SpectralTest do
+  use ExUnit.Case
+  doctest Spectral
+
+  test "greets the world" do
+    assert Spectral.hello() == :world
+  end
+end
